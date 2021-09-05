2bea709046c63e4b963a66258b290700db5b0c57
Qeuis <dyu5520@gmail.com>
2021-09-05 03:31:59 -0400

    Recommit with changes comments to include pseudocode

--- src/main/java/Solution01.java
+++ src/main/java/Solution01.java
@@ -3,22 +3,24 @@
  *  Copyright 2021 Danny Yu
  */
 
-// Create program that prompts user for name, and prints a greeting back to the user
+// Program wil prompt user for name and print a greeting back to the user
 
 import java.util.Scanner;
 
 public class Solution01
 {
 
+    /*
+        prompt user for name
+            read input from user
+        prints user input and greeting
+     */
+
     public static void main(String [] args)
     {
         Scanner in = new Scanner(System.in);
-        // variable to get user input
         System.out.print("Hello, what is your name?\n");
-        // prompt user for input (name)
         String name = in.nextLine();
-        // read input from user
         System.out.println("Hey " + name + ", it's nice to meet you!");
-        // print out input using standard output
     }
 }
